daffc9e457c29fc94e360e56dc7a9a5867787870
Alisher <alishzhangbyrshy@gmail.com>
2021-02-24 02:44:26 +0600

    finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'linebreak-style': 0,
+    'semi': 'off',
+    'object-curly-spacing': 0,
+    'padded-blocks': 0,
+    'comma-dangle': 0,
+    'no-trailing-spaces': 0,
+    'indent': 0,
+    'max-len': 1000
+  },
+};
